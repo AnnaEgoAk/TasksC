5725755a69232684fc0fe2258759820c69353feb
Anna <www.anne74@bk.ru>
2023-01-29 13:46:25 +0500

    поправка

--- Задача14_семинар2/Program.cs
+++ Задача14_семинар2/Program.cs
@@ -1,13 +1,15 @@
 ﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 
 Console.Clear();
-Console.WriteLine("Введите число: ");
 
-int a = int.Parse(Console.ReadLine());
-if((a % 7 == 0) && (a % 23 == 0));
-{
-    Console.WriteLine("да");
-}
+ Console.WriteLine("введите число");
+
+ int a = int.Parse(Console.ReadLine());
+ if (( a % 7 == 0)&&( a % 23 ==0))
+ {
+    Console.WriteLine("Да");
+ }
 else 
 {
     Console.WriteLine("нет");
+
 }
