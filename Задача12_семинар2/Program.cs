b46ca8ed21864afa1671e962926710d384cf81fe
Anna <www.anne74@bk.ru>
2023-01-29 13:20:33 +0500

    задача 12

---
+++ Задача12_семинар2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99]
+// и показывает наибольшую цифру числа.
+
+Console.Clear();
+
+ int first = new Random().Next(1,100);
+ int second = new Random().Next(1,100);
+ int ost = first % second;
+if (ost == 0) Console.WriteLine($"Число {first} кратное {second}");
+else Console.WriteLine($"Остаток от деления {ost}");
+
+
