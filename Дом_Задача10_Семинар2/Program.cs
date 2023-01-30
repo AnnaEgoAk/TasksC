302e23afff2ba60f41109817aebcb0bab6c358d9
Anna <www.anne74@bk.ru>
2023-01-30 09:29:08 +0500

    задача 10

---
+++ Дом_Задача10_Семинар2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.Clear();
+Console.Write("Введи трёхзначное число: ");
+int threeDigitNum = Convert.ToInt32(Console.ReadLine());
+string stringNum = Convert.ToString(threeDigitNum);
+Console.WriteLine("вторая цифра этого числа: "+stringNum[1]);
