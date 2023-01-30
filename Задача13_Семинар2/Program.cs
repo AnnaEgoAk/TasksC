587007bf65cca256254db0122cf729088cb1a23f
Anna <www.anne74@bk.ru>
2023-01-30 09:46:55 +0500

    задача 13 домашняя работа

---
+++ Задача13_Семинар2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.Clear();
+Console.Write("Введите число: ");
+int alsoNum = Convert.ToInt32(Console.ReadLine());
+string anyNum = Convert.ToString(alsoNum);
+if (anyNum.Length > 2){
+  Console.WriteLine("третья цифра: " + anyNum[2]);
+}
+else {
+  Console.WriteLine("третьей цифры нет");
+}
