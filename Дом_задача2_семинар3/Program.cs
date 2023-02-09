2cdcc839fdc67fa5033dca1d81389bdfa3b298bf
Anna <www.anne74@bk.ru>
2023-02-09 11:26:07 +0500

    4535

---
+++ Дом_задача2_семинар3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Clear();
+int num = 99;
+int A = 1;
+int cub = 1;
+while(A < num);
+{
+    cub = A * A * A;
+        if (cub % 2 == 0);
+        {
+            Console.Write($"{cub} ");
+        }
+    A++;    
+}
