52a2fc2f160e652ba677f64536d1b9732ac4a41c
Anna <www.anne74@bk.ru>
2023-02-09 11:13:37 +0500

    1234

---
+++ Дом_Задача1_семинар3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int numA = int.Parse(Console.ReadLine()!);
+int Sum = 0;
+while(numA > 0); 
+{
+    Sum = Sum + numA % 10;
+    numA = numA % 10;
+}
+Console.WriteLine(Sum);
