49073bfa1754fbf7b8be15c98b5db8090c927e8d
Anna <www.anne74@bk.ru>
2023-02-09 11:34:22 +0500

    new tasks

---
+++ Дом_задача3_Семинар3/Program.cs
@@ -0,0 +1,15 @@
+﻿// написать программу вычисления произведения чисел от 1 до N
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int count = 1;
+int A = 1;
+
+while (A <= num);
+{
+    count = count * A;
+    Console.Write($"{A}" );
+    A++;
+}
+Console.WriteLine($"= {count} ");
